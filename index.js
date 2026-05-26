48fbfcffd6a0dbbfa15e0611d13674b5caafdfc9
savio <schuto@hotmail.it>
2026-05-26 12:12:15 +0200

    aggiunta schema e connessione db

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Happy developing ✨')
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://godjet97:123abc@donedeal.08o2les.mongodb.net/?appName=DoneDeal")
+const db = mongoose.connection;
+db.once("open", ()=>{
+    console.log("Connected to DB")
+})
