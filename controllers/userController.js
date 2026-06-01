db27eabb1afb141fc49fecdd6f059cde5ec2c57f
Nicola <Utente@DESKTOP-5NA3PD0.Nicola>
2026-06-01 17:55:01 +0200

    modifiche

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,43 +5,57 @@ module.exports= {
         const data = req.body;
         try {
             const existingUser = await User.findOne({email: data.email})
-            existingUser ? res.status(409).json({message: "Utente gia registrato"}) :
-                newUser = new User({
-                    username: data.username,
-                    password: data.password,
-                    name: data.name,
-                    lastname: data.lastname,
-                    address: data.address,
-                    city: data.city,
-                    age: data.age,
-                    email: data.email,
-                    phone: data.phone,
-                    balance: data.balance,
-                    role: data.role,
-                    dealer: data.dealerData,
-                    services: data.services,
-                })
+            if(existingUser) {
+                return res.status(409).json({message: "Utente gia registrato"})
+            }
+                
+            newUser = new User({
+                username: data.username,
+                password: data.password,
+                name: data.name,
+                lastname: data.lastname,
+                address: data.address,
+                city: data.city,
+                age: data.age,
+                email: data.email,
+                phone: data.phone,
+                balance: data.balance,
+                role: data.role,
+                dealer: data.dealerData,
+                services: data.services,
+            });
+            
             await newUser.save()
-            res.status(201).json({message: "Utente registrato"})
+            return res.status(201).json({message: "Utente registrato"})
         } catch (err) {
-            res.status(500).json({message: "Qualcosa è andato storto" + err})
+            return res.status(500).json({message: "Qualcosa è andato storto" + err})
         }
-
     },
+
     getAllUsers: async function (req, res) {
-         try {
+        try {
              const users = await User.find()
-             users? res.status(200).json(users) : res.status(404).json({message: "Nessun utente trovato "})
-         }
-         catch (err) {
-             res.status(500).json({message: "Qualcosa è andato storto" + err})
-         }
+                if(users.length !==0) { 
+                    res.status(200).json(users)
+                }
+                else {
+                res.status(404).json({message: "Nessun utente trovato "})
+                }
+            }
+        catch (err) {
+                res.status(500).json({message: "Qualcosa è andato storto" + err})
+        }
     },
 
     getAllDealers: async function(req,res){
          try {
              const dealers = await User.find({role: "dealer"})
-             dealers.length !==0 ? res.status(200).json(dealers) : res.status(404).json({message: "Nessun fornitore trovato"})
+             if(dealers.length !==0) { 
+                res.status(200).json(dealers)
+            }
+              else {
+                res.status(404).json({message: "Nessun fornitore trovato"})
+            }
          }
          catch (err) {
              res.status(500).json({message: "Qualcosa è andato storto" + err})
@@ -52,10 +66,13 @@ module.exports= {
         const id = req.params._id
         try{
          const user = await User.findById({_id: id})
-             user ? res.status(200).json(user) : res.status(404).json({message: "Utente inesistente"})
+             if(!user){
+                return res.status(404).json({message: "Utente inesistente"});
+                }
+             return res.status(200).json(user);
          }
          catch(err){
-             res.status(500).json({message: "Qualcosa è andato storto" + err})
+             return res.status(500).json({message: "Qualcosa è andato storto" + err})
          }
     },
 
@@ -64,12 +81,12 @@ module.exports= {
         const id = req.params._id
         try {
             const user = await User.findById({_id: id})
-            if (!user) res.status(404).json({message: "Utente non trovato"})
+            if (!user) {return res.status(404).json({message: "Utente non trovato"})}
             else {
                 await User.updateOne(
                     {_id: req.params._id},
                     {$set: data})
-                res.status(200).json({message: "Modifica effettuata con successo"})
+                return res.status(200).json({message: "Modifica effettuata con successo"})
             }
         }
      catch(err){ res.status(500).json({message: "Qualcosa è andato storto" + err})
